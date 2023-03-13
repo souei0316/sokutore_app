08a14027cf4a4e2aa52066ad3e95baf5d51991a7
souei0316 <122407435+souei0316@users.noreply.github.com>
2023-03-13 22:47:13 +0900

    掲示板投稿〜削除まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
  devise_for :users
  root to:'homes#top'
+ resources :bbs_titles, only: [:new, :create, :index, :show, :destroy]
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_025652) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_053056) do
+  create_table "bbs_titles", charset: "utf8mb4", force: :cascade do |t|
+    t.text "title"
+    t.string "image_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

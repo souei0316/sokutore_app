32f498d5577935783284b95a0e320f6cf645ee5f
souei0316 <kumamon0136w@gmail.com>
2023-04-11 06:37:04 +0000

    画像挿入完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,47 @@
 TrainingColumn.create!(
-   content: '筋トレすると、寝起きが良くなります'
+   content: '血行が良くなり、こり・むくみ・冷えが改善されます'
 )
 
 TrainingColumn.create!(
-  content: '筋トレすると元気になります'
+   content: '体と脳の程よい疲労感から、睡眠の質が上がります'
 )
 
 TrainingColumn.create!(
-  content: '筋トレすると力がつきます'
+   content: 'セロトニンが分泌され、ストレスが解消されます'
+)
+
+TrainingColumn.create!(
+   content: '基礎代謝が上がり、痩せやすい体になります'
+)
+
+TrainingColumn.create!(
+   content: 'ストレッチをするのと同じくらい体が柔らかくなります'
+)
+
+TrainingColumn.create!(
+   content: '成長ホルモンの分泌が促され、免疫力が上がります'
+)
+
+TrainingColumn.create!(
+   content: '姿勢が良くなり、QOLの向上につながります'
+)
+
+TrainingColumn.create!(
+   content: '炎症の原因になる、ストレスへの耐性がつきます'
+)
+
+TrainingColumn.create!(
+   content: '成長ホルモンの分泌が促され、美容・アンチエイジングに良い影響があります'
+)
+
+TrainingColumn.create!(
+   content: '体が変わり、自分に自信を持てます'
+)
+
+TrainingColumn.create!(
+   content: 'テストステロンの分泌により、ポジティブになります'
+)
+
+TrainingColumn.create!(
+   content: '有酸素運動の効果も高まり、ダイエットの結果が出やすくなります'
 )
